511f8b178b7b7ded848dc9128e43c0ec81495681
djetchev <dimitar@inpher.io>
2024-05-07 00:29:12 +0200

    adding .gitignore in blueprints

--- CyclotomicUnits.lean
+++ CyclotomicUnits.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `CyclotomicUnits` library.
 -- Import modules here that should be built as part of the library.
-import «CyclotomicUnits».Basic
+import CyclotomicUnits.Basic
+import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.Basic
