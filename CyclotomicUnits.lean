483458f6fb82704a8aaef15087cf0703e798567e
djetchev <dimitar@inpher.io>
2024-05-07 07:29:39 +0200

    added support for Smith normal form

--- CyclotomicUnits.lean
+++ CyclotomicUnits.lean
@@ -1,6 +1,6 @@
 -- This module serves as the root of the `CyclotomicUnits` library.
 -- Import modules here that should be built as part of the library.
-import CyclotomicUnits.Basic
+import CyclotomicUnits.Regulator
 import Mathlib.NumberTheory.NumberField.CanonicalEmbedding.Basic
 import Mathlib.NumberTheory.NumberField.ClassNumber
 import Mathlib.NumberTheory.Cyclotomic.Basic
